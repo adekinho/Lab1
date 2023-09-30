18349d26a7f288ad847f0eacc52a571326a91c16
Adilet <117200205+adekinho@users.noreply.github.com>
2023-09-30 12:50:43 +0200

    Update Lab1-1.kt

--- algorithm1/Lab1-1.kt
+++ algorithm1/Lab1-1.kt
@@ -1,4 +1,4 @@
-fun main(args: Array<String>) {
+fun main(args: Array<String>) { //Adilet Mukhamedgalym
     println("Enter the length of the array A: " ) //Sprashivaem dlinnu massiva A
     val m: Int = readlnOrNull()?.toInt() ?: 0 //readlnOrNull (funkcia protiv nullPointerExeption) ?: (Vse chto null prevrashayet v 0)
 
